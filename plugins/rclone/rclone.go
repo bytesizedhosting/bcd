7140e241c3ed849145cbdba0df21646ffad9689c
Dedsec1 <jclark295@students.ccsnh.edu>
2016-11-08 14:43:58 -0500

    Update rclone.go

--- plugins/rclone/rclone.go
+++ plugins/rclone/rclone.go
@@ -1,89 +1,90 @@
+
 package rclone
 
 import (
-	log "github.com/Sirupsen/logrus"
-	"github.com/fsouza/go-dockerclient"
-	"github.com/bytesizedhosting/bcd/plugins"
-	"net/rpc"
-	"os"
+        log "github.com/Sirupsen/logrus"
+        "github.com/fsouza/go-dockerclient"
+        "github.com/bytesizedhosting/bcd/plugins"
+        "net/rpc"
+        "os"
 )
 
 type rclone struct {
-	plugins.Base
-	imageName string
+        plugins.Base
+        imageName string
 }
 
 func New(client *docker.Client) (*rclone, error) {
-	manifest, err := plugins.LoadManifest("rclone")
+        manifest, err := plugins.LoadManifest("rclone")
 
-	if err != nil {
-		return nil, err
-	}
+        if err != nil {
+                return nil, err
+        }
 
-	return &rclone{Base: plugins.Base{DockerClient: client, Name: "rclone", Version: 1, Manifest: manifest}, imageName: "bytesized/sonarr"}, nil
+        return &rclone{Base: plugins.Base{DockerClient: client, Name: "rclone", Version: 1, Manifest: manifest}, imageName: "bytesized/sonarr"}, nil
 }
 
 func (self *rclone) RegisterRPC(server *rpc.Server) {
-	rpc := plugins.NewBaseRPC(self)
-	server.Register(&rcloneRPC{base: self, BaseRPC: *rpc})
+        rpc := plugins.NewBaseRPC(self)
+        server.Register(&rcloneRPC{base: self, BaseRPC: *rpc})
 }
 
 type SonarrOpts struct {
-	plugins.BaseOpts
+        plugins.BaseOpts
 }
 
 func (self *rclone) Install(opts *rcloneOpts) error {
-	var err error
+        var err error
 
-	err = opts.SetDefault(self.Name)
-	if err != nil {
-		return err
-	}
+        err = opts.SetDefault(self.Name)
+        if err != nil {
+                return err
+        }
 
-	log.WithFields(log.Fields{
-		"plugin":       self.Name,
-		"datafolder":   opts.DataFolder,
-		"configfolder": opts.ConfigFolder,
-		"media_folder": opts.MediaFolder,
-	}).Debug("Sonarr options")
+        log.WithFields(log.Fields{
+                "plugin":       self.Name,
+                "datafolder":   opts.DataFolder,
+                "configfolder": opts.ConfigFolder,
+                "media_folder": opts.MediaFolder,
+        }).Debug("Sonarr options")
 
-	err = os.MkdirAll(opts.ConfigFolder, 0755)
-	if err != nil {
-		return err
-	}
+        err = os.MkdirAll(opts.ConfigFolder, 0755)
+        if err != nil {
+                return err
+        }
 
-	log.Debugln("Pulling docker image", self.imageName)
-	err = self.DockerClient.PullImage(docker.PullImageOptions{Repository: self.imageName}, docker.AuthConfiguration{})
-	if err != nil {
-		return err
-	}
+        log.Debugln("Pulling docker image", self.imageName)
+        err = self.DockerClient.PullImage(docker.PullImageOptions{Repository: self.imageName}, docker.AuthConfiguration{})
+        if err != nil {
+                return err
+        }
 
-	portBindings := map[docker.Port][]docker.PortBinding{
-		"8989/tcp": []docker.PortBinding{docker.PortBinding{HostPort: opts.WebPort}},
-	}
+        portBindings := map[docker.Port][]docker.PortBinding{
+                "8989/tcp": []docker.PortBinding{docker.PortBinding{HostPort: opts.WebPort}},
+        }
 
-	hostConfig := docker.HostConfig{
-		PortBindings: portBindings,
-		Binds:        plugins.DefaultBindings(opts),
-	}
+        hostConfig := docker.HostConfig{
+                PortBindings: portBindings,
+                Binds:        plugins.DefaultBindings(opts),
+        }
 
-	conf := docker.Config{Env: []string{"PUID=" + opts.User.Uid}, Image: self.imageName}
+        conf := docker.Config{Env: []string{"PUID=" + opts.User.Uid}, Image: self.imageName}
 
-	log.Debugln("Creating docker container")
-	c, err := self.DockerClient.CreateContainer(docker.CreateContainerOptions{Config: &conf, HostConfig: &hostConfig, Name: "bytesized_rclone_" + opts.WebPort})
+        log.Debugln("Creating docker container")
+        c, err := self.DockerClient.CreateContainer(docker.CreateContainerOptions{Config: &conf, HostConfig: &hostConfig, Name: "bytesized_rclone_" + opts.WebPort})
 
-	if err != nil {
-		return err
-	}
+        if err != nil {
+                return err
+        }
 
-	log.Debugln("Starting docker container")
+        log.Debugln("Starting docker container")
 
-	err = self.DockerClient.StartContainer(c.ID, nil)
-	if err != nil {
-		return err
-	}
+        err = self.DockerClient.StartContainer(c.ID, nil)
+        if err != nil {
+                return err
+        }
 
-	opts.ContainerId = c.ID
+        opts.ContainerId = c.ID
 
-	return nil
+        return nil
 }
